4b7b144efe35fad43ba96ecd8eb1757872da093d
Ravishankar M <raviennumnaan@gmail.com>
2024-01-24 09:38:26 +0530

    first commit

--- server.mjs
+++ server.mjs
@@ -5,6 +5,8 @@ const DB = process.env.DATABASE_URL.replace(
   "<PASSWORD>",
   process.env.DATABASE_PASSWORD
 );
+console.log(DB);
+
 mongoose.connect(DB).then(() => console.log("DB connection successfull"));
 
 const port = process.env.PORT || 3000;
